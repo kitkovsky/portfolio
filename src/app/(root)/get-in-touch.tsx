2563b8567771be16b2cfe306880aa1d1cc74d1ba
kitkovsky <okitkowski114@gmail.com>
2024-03-04 15:30:54 +0100

    add get in touch button

--- src/app/(root)/get-in-touch.tsx
+++ src/app/(root)/get-in-touch.tsx
@@ -1,7 +1,17 @@
+import { Link } from '@components/ui/link'
+import { EnvelopeIcon } from '@heroicons/react/24/outline'
+
 export const GetInTouch = (): React.ReactElement => (
   <div id="contact" className="scroll-mt-28">
     <h2 className="mb-7 text-2xl font-semibold">Get in touch</h2>
 
-    <p>please</p>
+    <Link
+      variant="default"
+      href="mailto:okitkowski114@gmail.com"
+      className="flex items-center justify-center font-semibold"
+    >
+      <EnvelopeIcon className="mr-2 h-6 w-6" />
+      Send me an email!
+    </Link>
   </div>
 )
